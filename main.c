c9c1167e30ac27d667e2f6e83096b4830611d037
i507abe <138637789+i507abe@users.noreply.github.com>
2023-07-13 15:48:47 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+#include <stdlib.h>　　//malloc関数とfree関数を使用するため
+#include <math.h>　　　//sqrt関数を使用するため
 
-double myRoot(double x) // x の平方根を返す関数。x >= 0 ではないとき exit(1) とする。
+double myRoot(double x)
 {
-  if (x < 0) {
+  if (x < 0) {　　　　//0も計算させるため
     exit(1);
   }
-  return sqrt(x);
+  return sqrt(x);　　　//戻り値は平方根の計算結果であり倍精度実数型でなければならないため
 }
 
 int main(void)
 {
-  int i = 0;
-  double *x = (double *)malloc(sizeof(double) * 101); // 101要素の配列を確保する
+  int i = 0;　　　　　//変数iを初期化するため
+  double *x = (double *)malloc(sizeof(double) * 101); // 0~100までなので101要素の配列を確保する必要があるため
 
   while (i <= 100) {
     x[i] = myRoot((double)i);
     i++;
   }
 
-  //以下は確認用
   printf("x[0] = %f\n", x[0]);
   printf("x[1] = %f\n", x[1]);
   printf("x[2] = %f\n", x[2]);
