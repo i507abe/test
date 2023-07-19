8d68a53e523c1aaf88a97b0bbe23abee3fdd9d14
i507abe <138637789+i507abe@users.noreply.github.com>
2023-07-19 10:43:39 +0900

    Update a.php

--- a.php
+++ a.php
@@ -1,7 +1,7 @@
 #!/usr/bin/php
 <?php
 foreach (file('/usr/share/dict/words') as $line) {
-    if (strpos($line, "abc") !== false) {　　　//strpos() の戻り値が false でない場合（つまり、"abc" を含む場合）
+    if (strpos($line, "xy") !== false) {　　　//strpos() の戻り値が false でない場合（つまり、"xy" を含む場合）
         echo $line;
     }
 }
